bebab2a54702bd95d0d9972ee56e39230c99ac36
rocco <lclaa@uade.edu.ar>
2024-12-16 23:31:39 -0300

    first push

---
+++ DeclaracionDeVariables.C
@@ -0,0 +1,13 @@
+char unChar; 
+float numeroFloat; 
+int numeroEntero; 
+float estoEsIgualQueEnJava; 
+
+#include <stdio.h> // Incluye la biblioteca estándar para printf
+#include <stdbool.h> // incluye los booleanos
+
+int main() {
+    bool esFumador; 
+    printf("%.2f", estoEsIgualQueEnJava);
+    return 0;
+}
---
+++ numero.py
@@ -0,0 +1,3 @@
+num = "1189731" + "0" * 4926
+print(num)
+# esta es la cantidad de digitos que puede almacenar un long double en C y pesa 16 bytes
